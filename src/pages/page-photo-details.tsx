f2f6b59cb5b92fda742198a8167afb40687ad19b
Mariano Capiliku <marianocapiliku@gmail.com>
2025-09-09 17:16:00 +0100

    feat: add photos navigator component and update photo details page

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,13 +1,29 @@
 import { useParams } from "react-router";
 import Text from "../components/text";
+import Container from "../components/container";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 
 const PagePhotoDetails = () => {
   const { id } = useParams();
 
+  // Only for test Propouse
+  const isLoadingPhoto = false;
+  const photo = {} as Photo;
+
   return (
-    <>
-      <Text variant="heading-medium">Page photo details {id}</Text>
-    </>
+    <Container>
+      <header className=" flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text variant="heading-large">{photo.title}</Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+        <PhotosNavigator />
+      </header>
+    </Container>
   );
 };
 
