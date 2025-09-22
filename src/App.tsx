3485667be687cc125e08aa716a6df483b0e4ebdf
Mariano Capiliku <marianocapiliku@gmail.com>
2025-09-22 16:35:43 +0100

    chore: commit latest changes to package files and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { RouterProvider } from "react-router";
 import { route } from "./routes/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
-  return <RouterProvider router={route} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={route} />
+    </QueryClientProvider>
+  );
 }
